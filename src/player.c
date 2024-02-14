19d8dbace2d64c10627d98cfb21b10701fd451ce
Dylan Miller <dylan.j.mill@gmail.com>
2024-02-14 16:46:10 -0700

    initial commit

---
+++ src/player.c
@@ -0,0 +1,70 @@
+#include <gst/gst.h>
+#include <gst/gstclock.h>
+#include <string.h>
+
+
+// static void cb_message (GstBus *bus, GstMessage *msg, CustomData *data) {
+//
+//   switch (GST_MESSAGE_TYPE (msg)) {
+//     case GST_MESSAGE_ERROR: {
+//       GError *err;
+//       gchar *debug;
+//
+//       gst_message_parse_error (msg, &err, &debug);
+//       g_print ("Error: %s\n", err->message);
+//       g_error_free (err);
+//       g_free (debug);
+//
+//       gst_element_set_state (data->pipeline, GST_STATE_READY);
+//       g_main_loop_quit (data->loop);
+//       break;
+//     }
+//     case GST_MESSAGE_EOS:
+//       /* end-of-stream */
+//       gst_element_set_state (data->pipeline, GST_STATE_READY);
+//       g_main_loop_quit (data->loop);
+//       break;
+//     default:
+//       /* Unhandled message */
+//       break;
+//     }
+// }
+
+int main(int argc, char *argv[]) {
+  GstElement *pipeline;
+  GstBus *bus;
+  GstStateChangeReturn ret;
+  GMainLoop *main_loop;
+
+  /* Initialize GStreamer */
+  gst_init (&argc, &argv);
+
+  /* Build the pipeline */
+  pipeline = gst_parse_launch ("rtspsrc location=rtsp://127.0.0.1:8554/test latency=0 do-retransmission=false is-live=true  debug=true ! rtph264depay ! h264parse ! avdec_h264 ! autovideosink sync=false", NULL);
+  bus = gst_element_get_bus (pipeline);
+
+
+  /* Start playing */
+  ret = gst_element_set_state (pipeline, GST_STATE_PLAYING);
+  if (ret == GST_STATE_CHANGE_FAILURE) {
+    g_printerr ("Unable to set the pipeline to the playing state.\n");
+    gst_object_unref (pipeline);
+    return -1;
+  } else if (ret == GST_STATE_CHANGE_NO_PREROLL) {
+    g_print("live stream\n");
+  }
+
+  main_loop = g_main_loop_new (NULL, FALSE);
+
+  gst_bus_add_signal_watch (bus);
+  // g_signal_connect (bus, "message", G_CALLBACK (cb_message), &data);
+
+  g_main_loop_run (main_loop);
+
+  /* Free resources */
+  g_main_loop_unref (main_loop);
+  gst_object_unref (bus);
+  gst_element_set_state (pipeline, GST_STATE_NULL);
+  gst_object_unref (pipeline);
+  return 0;
+}
